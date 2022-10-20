5d8586246e520cbdea0dfd8a2bed26deb3c3738c
Lopez Gonzalo <lopezgonzalo.jsx@gmail.com>
2022-10-20 00:46:22 -0300

    App working

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,31 @@
-import './App.css';
-import { useState } from 'react';
-import { Connection, PublicKey } from '@solana/web3.js';
-import {
-  Program, Provider, web3
-} from '@project-serum/anchor';
-import idl from './idl.json';
+import "./App.css";
+import { useState } from "react";
+import { Connection, PublicKey } from "@solana/web3.js";
+import { Program, Provider, web3 } from "@project-serum/anchor";
+import idl from "./idl.json";
 
-import { getPhantomWallet } from '@solana/wallet-adapter-wallets';
-import { useWallet, WalletProvider, ConnectionProvider } from '@solana/wallet-adapter-react';
-import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { getPhantomWallet } from "@solana/wallet-adapter-wallets";
+import {
+  useWallet,
+  WalletProvider,
+  ConnectionProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
 
 const wallets = [
   /* view list of available wallets at https://github.com/solana-labs/wallet-adapter#wallets */
-  getPhantomWallet()
-]
+  getPhantomWallet(),
+];
 
 const { SystemProgram, Keypair } = web3;
 /* create an account  */
 const baseAccount = Keypair.generate();
 const opts = {
-  preflightCommitment: "processed"
-}
+  preflightCommitment: "processed",
+};
 const programID = new PublicKey(idl.metadata.address);
 
 function App() {
@@ -33,14 +38,12 @@ function App() {
     const network = "http://127.0.0.1:8899";
     const connection = new Connection(network, opts.preflightCommitment);
 
-    const provider = new Provider(
-      connection, wallet, opts.preflightCommitment,
-    );
+    const provider = new Provider(connection, wallet, opts.preflightCommitment);
     return provider;
   }
 
-  async function createCounter() {    
-    const provider = await getProvider()
+  async function createCounter() {
+    const provider = await getProvider();
     /* create the program interface combining the idl, program ID, and provider */
     const program = new Program(idl, programID, provider);
     try {
@@ -51,11 +54,13 @@ function App() {
           user: provider.wallet.publicKey,
           systemProgram: SystemProgram.programId,
         },
-        signers: [baseAccount]
+        signers: [baseAccount],
       });
 
-      const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-      console.log('account: ', account);
+      const account = await program.account.baseAccount.fetch(
+        baseAccount.publicKey
+      );
+      console.log("account: ", account);
       setValue(account.count.toString());
     } catch (err) {
       console.log("Transaction error: ", err);
@@ -67,40 +72,42 @@ function App() {
     const program = new Program(idl, programID, provider);
     await program.rpc.increment({
       accounts: {
-        baseAccount: baseAccount.publicKey
-      }
+        baseAccount: baseAccount.publicKey,
+      },
     });
 
-    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-    console.log('account: ', account);
+    const account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
+    console.log("account: ", account);
     setValue(account.count.toString());
   }
 
   if (!wallet.connected) {
     /* If the user's wallet is not connected, display connect wallet button. */
     return (
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop:'100px' }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "100px",
+        }}
+      >
         <WalletMultiButton />
       </div>
-    )
+    );
   } else {
     return (
       <div className="App">
         <div>
-          {
-            !value && (<button onClick={createCounter}>Create counter</button>)
-          }
-          {
-            value && <button onClick={increment}>Increment counter</button>
-          }
+          {!value && <button onClick={createCounter}>Create counter</button>}
+          {value && <button onClick={increment}>Increment counter</button>}
 
-          {
-            value && value >= Number(0) ? (
-              <h2>{value}</h2>
-            ) : (
-              <h3>Please create the counter.</h3>
-            )
-          }
+          {value && value >= Number(0) ? (
+            <h2>{value}</h2>
+          ) : (
+            <h3>Please create the counter.</h3>
+          )}
         </div>
       </div>
     );
@@ -115,6 +122,6 @@ const AppWithProvider = () => (
       </WalletModalProvider>
     </WalletProvider>
   </ConnectionProvider>
-)
+);
 
 export default AppWithProvider;
