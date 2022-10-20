6a6bfc0d3d70667440ae624aaa9d15cf4499df4b
Lopez Gonzalo <lopezgonzalo.jsx@gmail.com>
2022-10-20 02:26:06 -0300

    Added Initialize function and their test

--- programs/mysolanaapp/src/lib.rs
+++ programs/mysolanaapp/src/lib.rs
@@ -1,10 +1,17 @@
 use anchor_lang::prelude::*;
 
-declare_id!("27cQpG8XWtgSyBiiexC7JX8p7cUM24LkWth9JgPzhyGe");
+declare_id!("2yr87xMTQZxR8JCF6xQR22Q5qh64qyhX1CvcCSWZGAZE");
 
 #[program]
 mod mysolanaapp {
     use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let program_account = &mut ctx.accounts.program_account;
+        program_account.count = 0;
+        program_account.bump = *ctx.bumps.get("program_account").unwrap();
+        Ok(())
+    }
 
     pub fn create(ctx: Context<Create>) -> Result<()> {
         let base_account = &mut ctx.accounts.base_account;
@@ -15,29 +22,48 @@ mod mysolanaapp {
     pub fn increment(ctx: Context<Increment>) -> Result<()> {
         let base_account = &mut ctx.accounts.base_account;
         base_account.count += 1;
+
+        let program_account = &mut ctx.accounts.program_account;
+        program_account.count += 1;
         Ok(())
     }
 }
 
-// Transaction instructions
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 8 + 1, seeds = [b"initialize".as_ref()], bump)]
+    pub program_account: Account<'info, ProgramAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
 #[derive(Accounts)]
 pub struct Create<'info> {
     #[account(init, payer = user, space = 16 + 16)]
     pub base_account: Account<'info, BaseAccount>,
     #[account(mut)]
     pub user: Signer<'info>,
-    pub system_program: Program <'info, System>,
+    pub system_program: Program<'info, System>,
 }
 
-// Transaction instructions
 #[derive(Accounts)]
 pub struct Increment<'info> {
     #[account(mut)]
     pub base_account: Account<'info, BaseAccount>,
+    #[account(mut, seeds = [b"initialize".as_ref()], bump = program_account.bump)]
+    pub program_account: Account<'info, ProgramAccount>,
 }
 
-// An account that goes inside a transaction instruction
 #[account]
+#[derive(Default)]
 pub struct BaseAccount {
     pub count: u64,
+}
+
+#[account]
+#[derive(Default)]
+pub struct ProgramAccount {
+    pub count: u64,
+    pub bump: u8,
 }
--- tests/mysolanaapp.js
+++ tests/mysolanaapp.js
@@ -1,14 +1,46 @@
 const assert = require("assert");
 const anchor = require("@project-serum/anchor");
-const { SystemProgram } = anchor.web3;
+const web3 = require("@solana/web3.js");
 
-describe("mysolanaapp", () => {
-  /* create and set a Provider */
+const { SystemProgram } = anchor.web3;
+const { PublicKey } = web3;
+
+describe("MySolanaApp", () => {
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
+  let _baseAccount;
+
   const program = anchor.workspace.Mysolanaapp;
-  it("Creates a counter)", async () => {
-    /* Call the create function via RPC */
+
+  it("Initialize", async () => {
+    const [programAccountPDA] = await PublicKey.findProgramAddress(
+      [anchor.utils.bytes.utf8.encode("initialize")],
+      program.programId
+    );
+
+    console.log("programAccountPDA: ", programAccountPDA.toString());
+
+    await program.methods
+      .initialize()
+      .accounts({
+        programAccount: programAccountPDA,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([])
+      .rpc({
+        commitment: "confirmed",
+      });
+
+    const account = await program.account.programAccount.fetch(
+      programAccountPDA
+    );
+
+    console.log("Count 0: ", account.count.toString());
+    assert.ok(account.count.toString() == 0);
+  });
+
+  it("Creates and initializes an account in a single atomic transaction (simplified)", async () => {
     const baseAccount = anchor.web3.Keypair.generate();
 
     await program.methods
@@ -18,12 +50,11 @@ describe("mysolanaapp", () => {
         user: provider.wallet.publicKey,
         systemProgram: SystemProgram.programId,
       })
-      .signers(baseAccount)
+      .signers([baseAccount])
       .rpc({
         commitment: "confirmed",
       });
 
-    /* Fetch the account and check the value of count */
     const account = await program.account.baseAccount.fetch(
       baseAccount.publicKey
     );
@@ -32,22 +63,70 @@ describe("mysolanaapp", () => {
     _baseAccount = baseAccount;
   });
 
-  it("Increments the counter", async () => {
+  it("Updates a previously created account", async () => {
+    // ACCOUNT 1
     const baseAccount = _baseAccount;
 
-    await program.methods
-      .increment()
-      .accounts({
+    const [programAccountPDA] = await PublicKey.findProgramAddress(
+      [anchor.utils.bytes.utf8.encode("initialize")],
+      program.programId
+    );
+
+    await program.rpc.increment({
+      accounts: {
         baseAccount: baseAccount.publicKey,
-      })
-      .rpc({
-        commitment: "confirmed",
-      });
+        programAccount: programAccountPDA,
+      },
+    });
 
     const account = await program.account.baseAccount.fetch(
       baseAccount.publicKey
     );
     console.log("Count 1: ", account.count.toString());
     assert.ok(account.count.toString() == 1);
+
+    // ACCOUNT 2
+    const baseAccount2 = anchor.web3.Keypair.generate();
+
+    await program.methods
+      .create()
+      .accounts({
+        baseAccount: baseAccount2.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([baseAccount2])
+      .rpc({
+        commitment: "confirmed",
+      });
+
+    let account2 = await program.account.baseAccount.fetch(
+      baseAccount2.publicKey
+    );
+    console.log("Count 0: ", account2.count.toString());
+    assert.ok(account2.count.toString() == 0);
+
+    await program.methods
+      .increment()
+      .accounts({
+        baseAccount: baseAccount2.publicKey,
+        programAccount: programAccountPDA,
+      })
+      .rpc({
+        commitment: "confirmed",
+      });
+
+    account2 = await program.account.baseAccount.fetch(baseAccount2.publicKey);
+
+    console.log("Count 1: ", account2.count.toString());
+    assert.ok(account2.count.toString() == 1);
+
+    // TOTAL PROGRAM COUNT
+    const accountPDA = await program.account.programAccount.fetch(
+      programAccountPDA
+    );
+
+    console.log("Count 2: ", accountPDA.count.toString());
+    assert.ok(accountPDA.count.toString() == 2);
   });
 });
